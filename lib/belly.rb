0a1f2d4bf139d112c74063111139913007985b41
Matt Wynne <matt@mattwynne.net>
2010-08-12 09:41:46 +0100

    Adding some basic error handling to handle being offline more elegantly

--- lib/belly.rb
+++ lib/belly.rb
@@ -7,11 +7,12 @@ require 'json'
 module Belly
   class << self
     def log(message)
-      return unless ENV['BELLY_DEBUG']
+      return unless ENV['BELLY_LOG']
       puts "* [Belly] #{message}"
     end
   
     def publish(scenario)
+      return if offline?
       feature_name = scenario.feature.name.split("\n").first # TODO: only needed for older cucumbers
       data = { 
         :type => :cucumber_scenario_result,
@@ -26,15 +27,39 @@ module Belly
       thread = Thread.new { hub.post_test_result(data) }
       
       # Make sure the thread gets a chance to finish before the process exits
-      at_exit { thread.join }
+      at_exit do
+        begin
+          thread.join
+        rescue SocketError => exception
+          # TODO: test this
+          unless offline?
+            warn("Belly couldn't save your results. Maybe you're offline?")
+            offline!
+          end
+        end
+      end
     end
   
     def config
       @config ||= Config.new
     end
     
+    def user_credentials
+      @user_credentials ||= UserCredentials.new(config)
+    end
+    
     def hub
       @hub ||= Belly::Client.new(config)
+    end
+    
+  private
+    
+    def offline?
+      @offline
+    end
+    
+    def offline!
+      @offline = true
     end
   end
 end
