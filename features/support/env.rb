a8e435b697f3d2d61400cd0d3c9c55b2fe6b4f18
Matt Wynne <matt@mattwynne.net>
2010-08-21 10:46:11 +0100

    Remove cruft

--- features/support/env.rb
+++ features/support/env.rb
@@ -4,7 +4,7 @@ require 'aruba'
 bin_dir = File.expand_path(File.dirname(__FILE__) + '/../../bin/')
 ENV["PATH"] = bin_dir + ":" + ENV["PATH"]
 
-require 'spec/expectations'
+# require 'spec/expectations'
 
 module BellyWorld
   def belly_lib_path
